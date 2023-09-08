5d8a4d128aa77688f8db5f08fa3062766cd9f8ba
Eazyisreal <eazyisreal192@gmail.com>
2023-09-08 01:39:55 +0100

    [Chore]: Re-evaluated task to properly count in milliseconds while updating UI

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 // Function to update the current day of the week and UTC time in milliseconds
-function updateData() {
-    const currentDate = new Date();
-    const currentMillis = currentDate.getTime(); // Get UTC time in milliseconds
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Current Day: ${currentDate.toLocaleDateString('en-US', { weekday: 'long' })}`;
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = `Current UTC Time (Milliseconds): ${currentMillis}`;
-}
-
-// Function to handle errors
-function handleErrors() {
-    const profileImage = document.querySelector('[data-testid="slackDisplayImage"]');
-    profileImage.onerror = () => {
-        profileImage.src = 'eazy.jpg'; // Replace with a default image
-        profileImage.alt = 'Default Profile';
-    };
-
-    const githubURL = document.querySelector('[data-testid="githubURL"]');
-    githubURL.onerror = () => {
-        githubURL.textContent = 'GitHub Repository Not Found'; // Provide error message
-        githubURL.href = '#'; // Remove the link
-    };
-}
-
-// Call the update and error handling functions
-updateData();
-handleErrors();
+function updateDateTime() {
+    const currentDayOfTheWeek = document.querySelector(
+      '[data-testid="currentDayOfTheWeek"]'
+    );
+    const currentUTCTime = document.querySelector(
+      '[data-testid="currentUTCTime"]'
+    );
+    const date = new Date();
+    const dayOfTheWeek = date.toLocaleString("en-us", { weekday: "long" });
+    const utcTime = date.getTime();
+  
+    currentDayOfTheWeek.textContent = dayOfTheWeek;
+    currentUTCTime.textContent = `Current UTC Time (Milliseconds): ${utcTime}`;
+  }
+  
+  // Call updateDateTime initially to set the initial values
+  updateDateTime();
+  
+  // Set up a timer to update the values every second (or adjust the interval as needed)
+  setInterval(updateDateTime, 1000); // 1000 milliseconds = 1 second
+  
+  // Function to handle errors
+  function handleErrors() {
+      const profileImage = document.querySelector('[data-testid="slackDisplayImage"]');
+      profileImage.onerror = () => {
+          profileImage.src = 'eazy.jpg'; // Replace with a default image
+          profileImage.alt = 'Default Profile';
+      };
+  
+      const githubURL = document.querySelector('[data-testid="githubURL"]');
+      githubURL.onerror = () => {
+          githubURL.textContent = 'GitHub Repository Not Found'; // Provide error message
+          githubURL.href = '#'; // Remove the link
+      };
+  }
+  
+  // Call the update and error handling functions
+  updateData();
+  handleErrors();
+  
